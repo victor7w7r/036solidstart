0aed875d8a78e28737f17bbed75c41b52d5b4c68
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2024-05-30 12:30:48 -0500

    PutBlankTemplate

--- src/app.tsx
+++ src/app.tsx
@@ -1,40 +1,21 @@
 import '~/core/styles/index.css'
-import '~/core/styles/global.css'
 
 import { MetaProvider, Title } from '@solidjs/meta'
 import { Router } from '@solidjs/router'
-import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
 import { FileRoutes } from '@solidjs/start/router'
 import { Suspense } from 'solid-js'
 
-import DataProvider from '~/features/common/ui/context/data-context'
-import ThemeProvider from '~/features/common/ui/context/theme-context'
-import { Header } from '~/features/common/ui/components'
-
-const queryClient = new QueryClient()
-
 export default function App() {
   return (
-    <ThemeProvider>
-      <DataProvider>
-        <QueryClientProvider client={queryClient}>
-          <Router
-            root={props => (
-              <MetaProvider>
-                <Title>036solidstart</Title>
-                <Suspense>
-                  <main class='flex h-screen flex-col gap-8'>
-                    <Header />
-                    {props.children}
-                  </main>
-                </Suspense>
-              </MetaProvider>
-            )}
-          >
-            <FileRoutes />
-          </Router>
-        </QueryClientProvider>
-      </DataProvider>
-    </ThemeProvider>
+    <Router
+      root={props => (
+        <MetaProvider>
+          <Title>036solidstart</Title>
+          <Suspense>{props.children}</Suspense>
+        </MetaProvider>
+      )}
+    >
+      <FileRoutes />
+    </Router>
   )
 }
--- src/features/home/business/repositories/binance-repository.ts
+++
@@ -1,5 +0,0 @@
-import type { Binance } from '~/features/home/business/models'
-
-export type BinanceRepository = Readonly<{
-  getBitcoin: () => Promise<Binance>
-}>
